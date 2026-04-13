050aef0fe92c01416670ce8c28c8a8d241fe5e7e
Dheeraj Tommandru <tommad01@mssm.edu>
2026-04-13 05:58:15 -0400

    Refreshed Queries

--- Validation.R
+++ Validation.R
@@ -64,7 +64,43 @@ room_exclusion_list <- "('MSW OR 23','MSM OR 08','MSM OR 15')"
 
 
 query <- glue("
-              SELECT *
+              SELECT ENCOUNTERS.OR_CASE_ID,
+                     ENCOUNTERS.ENCOUNTER_NO_SRC AS ENCOUNTER_NO_SRC,
+                     ENCOUNTERS.ENCOUNTER_ID AS ENCOUNTER_ID,
+                     ENCOUNTERS.PAYOR_GROUP_DESC_MSX_OP,
+                     ENCOUNTERS.CCM_PAYOR_DESC_MSX_OP,
+                     ENCOUNTERS.PAYOR_GROUP_OP,
+                     ENCOUNTERS.CLINIC_GROUP_DESC_MSX,
+                     ENCOUNTERS.REG_AREA_DESC_SRC,
+                     -- Provider Details
+                     ENCOUNTERS.ATTENDING_MD_NAME_MSX AS ATTENDING_MD,
+                     ENCOUNTERS.ATTENDING_MD_SPEC_SRC AS ATTENDING_MD_SPECIALIZATION,
+                     ENCOUNTERS.PRIMARY_SURGEON AS PRIMARY_SURGEON,
+                     ENCOUNTERS.SURGEON_SPECIALTY AS PRIMARY_SURGEON_SPECIALTY,
+                     -- Patient Details
+                     ENCOUNTERS.PAT_CLASS_NAME AS PATIENT_CLASS,
+                     ENCOUNTERS.PAT_MRN_ID AS PATIENT_MRN,
+                     TO_CHAR(ENCOUNTERS.PAT_DOB,'YYYY-MM-DD') AS PATIENT_DOB,
+                     ENCOUNTERS.ADMIT_CSN_ID,
+                     -- Procedure Details
+                     ENCOUNTERS.TOTAL_TIME_NEEDED,
+                     ENCOUNTERS.PRIMARY_PROC_CODE AS PRIMARY_PROCEDURE_CODE,
+                     ENCOUNTERS.PRIMARY_PROCEDURE AS PRIMARY_PROCEDURE_DESC,
+                     ENCOUNTERS.ANESTHESIA_TYPE AS ANESTHESIA_TYPE,
+                     -- Time Data/dates
+                     ENCOUNTERS.SCHED_IN_ROOM_DTTM,
+                     ENCOUNTERS.SCHED_START_TIME,
+                     ENCOUNTERS.PATIENT_IN_ROOM_DTTM,
+                     ENCOUNTERS.PATIENT_OUT_ROOM_DTTM,
+                     ENCOUNTERS.MINUTES_IN_ROOM_TO_OUT_ROOM,
+                     ENCOUNTERS.TURNOVER_FROM_PRIOR_CASE,
+                     ENCOUNTERS.SURGERY_DATE,
+                     -- Room/Location Details
+                     ROOM_MASTER.ROOM_ID AS ROOM_ID,
+                     ROOM_MASTER.LOCATION_NM AS LOCATION_NAME,
+                     ROOM_MASTER.CLUSTER_NAME AS CLUSTER_NAME,
+                     ROOM_MASTER.ROOM_NM AS ROOM_NAME,
+                     ENCOUNTERS.ROBOTIC_SURGERY_DAVINCI_YN
               FROM
               {encounter_data_table_name} ENCOUNTERS
               LEFT JOIN
@@ -86,5 +122,23 @@ query <- glue("
               ")
 
 
+room_schedules <- glue("
+                       SELECT LOG_ID AS OR_CASE_ID,
+                              ROOM_ID
+                              SNAPSHOT_DATE,
+                              ABSOLUTE_SLOT_START,
+                              ABSOLUTE_SLOT_END
+                       FROM {utlization_calculation_table} TEMPLATE_TIME
+                       WHERE
+                       TEMPLATE_TIME.SNAPSHOT_DATE >= TO_DATE('{sched_start_date}','YYYY-MM-DD') AND
+                       TEMPLATE_TIME.SNAPSHOT_DATE <= TO_DATE('{sched_end_date}','YYYY-MM-DD') AND
+                       TEMPLATE_TIME.WEEKEND_YN = 'N' AND
+                       TEMPLATE_TIME.HOLIDAY_YN = 'N' AND
+                       SLOT_TYPE = 'BLOCK';
+                       ")
+              
+
+
 schedule_data <- dbGetQuery(conn, query)
+room_schedules_data <- dbGetQuery(conn,room_schedules)
 dbDisconnect(conn)
